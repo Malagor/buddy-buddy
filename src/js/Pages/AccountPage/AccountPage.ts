6879f56aaa36545d8c9785a580599ce22afc447f
Darya Tsimashenkava (@besovadevka) <timaph@mail.ru>
2021-01-06 16:33:23 +0300

    fix: add new wrapper/ fix styles

--- src/js/Pages/AccountPage/AccountPage.ts
+++ src/js/Pages/AccountPage/AccountPage.ts
@@ -8,45 +8,47 @@ export class AccountPage extends Page {
 
   render = (data: any): void => {
     this.element.innerHTML = `
-    <div class="account__header account__header__scroll_out">
-      <div class="account__image_wrapper">
-        <img src="${data.avatar}" alt="${data.name}" class="account__image account__image__scroll_out">
+    <div class="account__wrapper">
+      <div class="account__header account__header__scroll_out">
+        <div class="account__image-wrapper">
+          <img src="${data.avatar}" alt="${data.name}" class="account__image account__image__scroll_out">
+        </div>
+        <p class="account__nick">@${data.id}</p>
+        <form action=#" enctype="multipart/form-data" method="post" class="account__form_change_photo">
+          <label for="file" class="account__button_change_photo">
+            <i class="material-icons">monochrome_photos</i>
+          </label>
+          <input type="file" name="avatar" id="file" class="account__input__photo">
+        </form>      
       </div>
-      <p class="account__nick">@${data.id}</p>
-      <form action=#" enctype="multipart/form-data" method="post" class="account__form_change_photo">
-        <label for="file" class="account__button_change_photo">
-          <i class="material-icons">monochrome_photos</i>
-        </label>
-        <input type="file" name="avatar" id="file" class="account__input__photo">
-      </form>      
+      <div class="account__info">
+        <form action=#" enctype="multipart/form-data" method="post" class="account__form_change_info">
+          <input type="text" value="${data.name}" name="name" class="account__info__input account__info__name" placeholder="Name Surname" required>
+          <ul class="account__info__list">
+          <li class="account__info__list__item">
+            <input type="email" value="user@mail.ru" name="email" class="account__info__input" placeholder="E-mail" required>
+          </li>
+          <li class="account__info__list__item">
+            <input type="date" value="21/05/1999" name="date" class="account__info__input" required placeholder="DD/MM/YYYY">
+          </li>
+          <li class="account__info__list__item">
+            <input type="submit" value="Save" class="account__input_submit account__input_submit_off account__input_submit_no_changes" disabled>
+          </li>
+        </form>  
+        <li class="account__info__list__item">
+          <button type="button" class="account__button_groups account__button">
+            My groups
+          </button>
+        </li>
+        <li class="account__info__list__item">
+          <button type="button" class="account__button_settings account__button">
+            Settings
+          </button>
+          </li>
+        </ul>
+      </div>
+      <p class="account__balance">Balance</p>
     </div>
-    <div class="account__info">
-      <form action=#" enctype="multipart/form-data" method="post" class="account__form_change_info">
-        <input type="text" value="${data.name}" name="name" class="account__info__input account__info__name" placeholder="Name Surname" required>
-        <ul class="account__info__list">
-        <li class="account__info__list__item">
-          <input type="email" value="user@mail.ru" name="email" class="account__info__input" placeholder="E-mail" required>
-        </li>
-        <li class="account__info__list__item">
-          <input type="date" value="21/05/1999" name="date" class="account__info__input" required placeholder="DD/MM/YYYY">
-        </li>
-        <li class="account__info__list__item">
-          <input type="submit" value="Save" class="account__input_submit account__input_submit_off account__input_submit_no_changes" disabled>
-        </li>
-      </form>  
-      <li class="account__info__list__item">
-        <button type="button" class="account__button_groups account__button">
-          My groups
-        </button>
-      </li>
-      <li class="account__info__list__item">
-        <button type="button" class="account__button_settings account__button">
-          Settings
-        </button>
-        </li>
-      </ul>
-    </div>
-    <p class="account__balance">Balance</p>
       `;
     this.events();
   };
