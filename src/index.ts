d1d0b0acb5700d7ab96831100fec60dc42696f01
Alex Malagor <seo.dnja@gmail.com>
2021-01-08 15:01:20 +0300

    feat: add tslint, configured cssComb script in package.json

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,4 @@
 import { App } from './js/Classes/App';
-// import {MDCTopAppBar} from '@material/top-app-bar';
-// import {MDCDrawer} from '@material/drawer';
-
-// import 'material-components-web';
-// import 'material-components-web/dist/material-components-web.min.css';
 
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -11,13 +6,4 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './scss/basic-styles.scss';
 import './js/Pages/stylePages';
 
-
-
 App.create();
-
-// const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
-// const topAppBar = MDCTopAppBar.attachTo(document.getElementById('app-bar'));
-// topAppBar.setScrollTarget(document.getElementById('main-content'));
-// topAppBar.listen('MDCTopAppBar:nav', () => {
-//   drawer.open = !drawer.open;
-// });
