09a935ddadfb137b5017fea369660f0e87698b1d
Darya Tsimashenkava (@besovadevka) <timaph@mail.ru>
2021-01-25 14:23:10 +0300

    feat: add funcs for chart/connecting styles for chart

--- src/js/Classes/ChartBar.ts
+++ src/js/Classes/ChartBar.ts
@@ -10,68 +10,106 @@ export class ChartBar {
 
   static create(element: string): ChartBar {
     const chart: ChartBar = new ChartBar(element);
-    chart.render = chart.render.bind(chart);
+    chart.buildingChart = chart.buildingChart.bind(chart);
     return chart;
   }
 
-  async getConfig(data: any[], groupData: any) {
+  buildingChart = (userList: [string | number]): void => {
+    console.log(userList);
+    const dt: any[]= [{userName: 'Darya', userId: '@daryatema', userBalance: 100, userAvatar: "https://firebasestorage.googleapis.com/v0/b/buddy-buddy-8e497.appspot.com/o/avatars%2F6yiqUegBoWWdR5oiZgTVqUt871q2.jpg?alt=media&token=d8308ef6-00d1-4ba7-aa77-eb396c46639e"},
+    {userName: 'Alex', userId: '@malagor', userBalance: 300, userAvatar: "https://firebasestorage.googleapis.com/v0/b/buddy-buddy-8e497.appspot.com/o/avatars%2F6yiqUegBoWWdR5oiZgTVqUt871q2.jpg?alt=media&token=d8308ef6-00d1-4ba7-aa77-eb396c46639e"},
+    {userName: 'Olga', userId: '@gruzyn33', userBalance: 100, userAvatar: "https://firebasestorage.googleapis.com/v0/b/buddy-buddy-8e497.appspot.com/o/avatars%2F6yiqUegBoWWdR5oiZgTVqUt871q2.jpg?alt=media&token=d8308ef6-00d1-4ba7-aa77-eb396c46639e"},
+    {userName: 'Andrei', userId: '@andrei', userBalance: -500, userAvatar: "https://firebasestorage.googleapis.com/v0/b/buddy-buddy-8e497.appspot.com/o/avatars%2F6yiqUegBoWWdR5oiZgTVqUt871q2.jpg?alt=media&token=d8308ef6-00d1-4ba7-aa77-eb396c46639e"},
+    {userName: 'Darya', userId: '@daryatema', userBalance: 100, userAvatar: "https://firebasestorage.googleapis.com/v0/b/buddy-buddy-8e497.appspot.com/o/avatars%2F6yiqUegBoWWdR5oiZgTVqUt871q2.jpg?alt=media&token=d8308ef6-00d1-4ba7-aa77-eb396c46639e"},
+    {userName: 'Alex', userId: '@malagor', userBalance: 300, userAvatar: "https://firebasestorage.googleapis.com/v0/b/buddy-buddy-8e497.appspot.com/o/avatars%2F6yiqUegBoWWdR5oiZgTVqUt871q2.jpg?alt=media&token=d8308ef6-00d1-4ba7-aa77-eb396c46639e"},
+  ];
+    const groupData = {curr: 'BYN', groupTitle: 'Our group'};
+    this.renderChart();
+    this.getDataForChartConfig(dt, groupData).then((data: any) => {
+      const config: any = this.getConfigForChart(data);
+      const chart: Chart = this.getChart(config);
+      this.getAvatarsLocation(chart, dt);
+      this.eventsForChart(chart);
+    });
+  }
+
+  renderChart = (): void => {
+    this.element.innerHTML = `
+    <div class="chart-wrapper">
+    <canvas id="ChartBar" width="100" height="60" class="ChartBar">
+    </canvas>
+    </div>
+    <div class="images-for-canvas">
+    </div>
+    `;
+  }
+
+  async getDataForChartConfig(data: any[], groupData: any) {
     const labelsOfUser: any[] = data.map((item: any) => [`${item.userName},`, `<${item.userId}>`]);
     const groupCurrency: string = groupData.curr;
+    const colors: string[] = this.getArrayOfColors(data);
+    const opacityColors: string[] = colors.map((item: string) => item + '80');
     const balancesArray: any = [];
     const query = Currencies.fromUSD(groupCurrency);
-    const balances: any[] = data.map((item: any) => item.userBalance);
-    const rightBalances: any = await balances.map(async (sum: number) => {
+    const rightBalances: any = await data.map((item: any) => item.userBalance)
+    .map(async (sum: number) => {
       await query(sum).then((bal: number) => {
         balancesArray.push(bal.toFixed(2));
-        return bal;
       });
     });
     await Promise.all(rightBalances).then(data => data);
 
+    return {
+      labelsOfUser: labelsOfUser,
+      groupCurrency: groupCurrency,
+      colors: colors,
+      opacityColors: opacityColors,
+      rightBalances: balancesArray,
+      groupTitle: groupData.groupTitle,
+    }
+  }
+
+  generateRandomColor(): string {
+    const letters: string = '0123456789ABCDEF';
+    let color: string = '#';    
+    for (let i = 0; i < 6; i += 1) {
+      color += letters[Math.floor(Math.random() * letters.length)];
+    }
+    return color;
+  }
+
+  getArrayOfColors(data: any[]): string[] {
+    const colorSet: Set<string> = new Set();
+    data.map((_: any) => {
+      let color: string;
+      do {
+        color = this.generateRandomColor();
+      } while (colorSet.has(color));
+
+      colorSet.add(color);
+    });
+
+    return Array.from(colorSet);
+  }
+
+  getConfigForChart(configData: any): any {
     const dataInfo: any = {
-      labels: labelsOfUser,
+      labels: configData.labelsOfUser,
       datasets: [{
-        label: "Dataset #1",
-        backgroundColor: balancesArray.map((item: number) => {
-          if (item > 0) {
-            return "rgba(0, 250, 0, 0.3)";
-          } else {
-            return "rgba(255, 0, 0, 0.3)";
-          }
-        }),
-        borderColor: balancesArray.map((item: number) => {
-          if (item > 0) {
-            return "rgba(0, 250, 0, 0.5)";
-          } else {
-            return "rgba(255, 0, 0, 0.5)";
-          }
-        }),
-        borderWidth: 2,
-        hoverBackgroundColor: balancesArray.map((item: number) => {
-          if (item > 0) {
-            return "rgba(0, 250, 0, 0.5)";
-          } else {
-            return "rgba(255, 0, 0, 0.5)";
-          }
-        }),
-        hoverBorderColor: balancesArray.map((item: number) => {
-          if (item > 0) {
-            return "rgba(0,100, 0, 0.8)";
-          } else {
-            return "rgba(255, 0, 0, 0.8)";
-          }
-        }),
-        data: balancesArray,
+        label: configData.groupTitle,
+        backgroundColor: configData.opacityColors,
+        hoverBackgroundColor: configData.colors,
+        data: configData.rightBalances,
         },]
       };
-      
-      const optionsInfo: any = {
-      maintainAspectRatio: false,
+    
+    const optionsInfo: any = {
+      maintainAspectRatio: true,
       responsive: true,
       title: {
         display: true,
-        text: groupData.groupTitle,
-        padding: 15,
+        text: `${configData.groupTitle} - ${configData.groupCurrency}`,
+        padding: 10,
       },
       legend: {
         display: false,
@@ -79,21 +117,22 @@ export class ChartBar {
       scales: {
         yAxes: [{
           ticks: {
-            callback(value: number) {
-              return `${value} ${groupCurrency}`;
-            },
-            maxTicksLimit: 10
+            maxTicksLimit: 10,
+            fontSize: 10,
           },
-          stacked: true,
           gridLines: {
             display: true,
             color: "rgba(0, 0, 0, 0.05)"
-          }
+          },
         }],
         xAxes: [{
+          ticks: {
+            display: false,
+            autoSkip: false,
+          },
           gridLines: {
             display: false
-          }
+          },
         }]
       },
       tooltips: {
@@ -109,12 +148,11 @@ export class ChartBar {
             return toolTipItems[0].xLabel.join('\n');            
           },
           label(toolTipItems: any) {
-            return `${toolTipItems.value} ${groupCurrency}`; 
+            return `${toolTipItems.value} ${configData.groupCurrency}`; 
           },
-        }
-
-      }
-      };
+        },
+      },
+    };
 
     return {
       type: 'bar',
@@ -123,32 +161,33 @@ export class ChartBar {
     }
   }
 
-  render = (userList: [string | number]): void => {
-    console.log(userList);
-    const dt: any[]= [{userName: 'Darya', userId: '@daryatema', userBalance: 100},
-    {userName: 'Alex', userId: '@malagor', userBalance: 300},
-    {userName: 'Olga', userId: '@gruzyn33', userBalance: 100},
-    {userName: 'Andrei', userId: '@andrei', userBalance: -500},
-    {userName: 'Darya', userId: '@daryatema', userBalance: 100},
-    {userName: 'Alex', userId: '@malagor', userBalance: 300},
-    {userName: 'Olga', userId: '@gruzyn33', userBalance: 100},
-    {userName: 'Andrei', userId: '@andrei', userBalance: -500},
-    {userName: 'Darya', userId: '@daryatema', userBalance: 100},
-    {userName: 'Alex', userId: '@malagor', userBalance: 300},
-    {userName: 'Olga', userId: '@gruzyn33', userBalance: 100},
-    {userName: 'Andrei', userId: '@andrei', userBalance: -500},
-  ];
-  const groupData = {curr: 'BYN', groupTitle: 'Our group'};
-    
-    this.element.innerHTML = `
-    <canvas id="ChartBar" width="100" height="300" class="ChartBar">
-    </canvas>`;
-
+  getChart(config: any): Chart {
     const canvas = document.getElementById('ChartBar') as HTMLCanvasElement;
     const ctx: CanvasRenderingContext2D = canvas.getContext('2d');
+    const myChart: Chart = new Chart(ctx, config);
 
-    const config: any = this.getConfig(dt, groupData).then(data => {
-      const myChart = new Chart(ctx, data);
+    return myChart;
+  }
+
+  getAvatarsLocation(chart: any, data: any[]) {
+    const xAxis: any = chart.scales['x-axis-0'];
+    xAxis.ticks.forEach((_: any, index: number) => {
+      const x = xAxis.getPixelForTick(index);
+      const html = `<div class="wr" style="left: ${x - 12}px;">
+        <img src="${data[0].userAvatar}">
+      </div>`;
+      document.querySelector('.images-for-canvas').insertAdjacentHTML('beforeend', html);
+    })
+  }
+
+  eventsForChart(chart: any): void {
+    window.addEventListener('resize', () => {
+      const avatars: any = document.querySelector('.images-for-canvas').querySelectorAll('.wr');
+      const xAxis: any = chart.scales['x-axis-0'];
+      xAxis.ticks.forEach((_: any, index: number) => {
+        const x = xAxis.getPixelForTick(index);
+        avatars[index].style.left = `${x - 12}px`;
+      });
     });
   }
 }
