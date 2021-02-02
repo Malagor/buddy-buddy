d1a7b12cfbe08a160c5097c237014d2b0bb8c403
Alex Malagor <seo.dnja@gmail.com>
2021-02-02 13:40:46 +0300

    fix: corrected the recipient's additions

--- src/js/Pages/Contacts/onClickContactInContactsList.ts
+++ src/js/Pages/Contacts/onClickContactInContactsList.ts
@@ -1,20 +1,19 @@
 export const onClickContactInContactsList = (selector: string | null = null): void => {
   const contactList = document.querySelector(`${selector ? selector : '.contacts-user-list'}`);
   contactList.addEventListener('click', event => {
-    console.log('onClickContactInContactsList');
 
     const { target }: any = event;
 
     if (target.closest('.contact-list__item')) {
       const item: HTMLElement = target.closest('.contact-list__item');
       const formRecipient: HTMLInputElement = document.querySelector('#activeContact');
-      const keyInput: HTMLInputElement = document.querySelector('.contact-user-id');
+      const idInput: HTMLInputElement = document.querySelector('.contact-user-id');
 
       const name = item.querySelector('.contact-list__name').textContent;
       const account = item.querySelector('.contact-list__account').textContent;
 
       formRecipient.value = `${name} ${account}`;
-      keyInput.value = item.getAttribute('data-user-id');
+      idInput.value = item.getAttribute('data-user-id');
     }
   });
 };
