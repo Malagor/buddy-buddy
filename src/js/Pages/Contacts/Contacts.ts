c9be749e0f62e885bd81fa97879b07125fcfb9c7
Darya Tsimashenkava (@besovadevka) <timaph@mail.ru>
2021-02-04 10:11:25 +0300

    fix: delece comments

--- src/js/Pages/Contacts/Contacts.ts
+++ src/js/Pages/Contacts/Contacts.ts
@@ -2,14 +2,6 @@ import { Page } from '../../Classes/Page';
 import { getFormData } from '../../Util/getFormData';
 import { Modal } from 'bootstrap';
 import { i18n } from '@lingui/core';
-import { messagesRU } from '../../languages/RU/messages';
-import { messagesENG } from '../../languages/ENG/messages';
-import { loadLanguage } from '../../Util/saveLoadLanguage';
-// i18n.load('RU', messagesRU);
-// i18n.load('ENG', messagesENG);
-
-// const locale = loadLanguage();
-// i18n.activate(locale);
 
 export interface ISearchUserData {
   name?: string;
